52a8848e2bbbf1d9cfab60829e7919fbdf0d3c6f
e1500948 <tynshjt@gmail.com>
2019-02-14 13:36:42 +0200

    fix firestore support

--- src/Database/Firestore.js
+++ src/Database/Firestore.js
@@ -1,13 +1,14 @@
 import * as firebase from 'firebase';
 import firestore from 'firebase/firestore'
+import config_ from './config_'
 
 require('firebase/firestore')
 
 const settings = {timestampsInSnapshots: true};
 var config = {
-    apiKey: "AIzaSyBqvvhFtFFzxH1O-Wi--eO08AYfFIg7bvw",
+    apiKey: config_.apiKey,
     authDomain: "bachtuoc193-de09e.firebaseapp.com",
-    databaseURL: "https://bachtuoc193-de09e.firebaseio.com",
+    databaseURL: config_.databaseURL,
     projectId: "bachtuoc193-de09e",
     storageBucket: "bachtuoc193-de09e.appspot.com",
     messagingSenderId: "636955558116"
